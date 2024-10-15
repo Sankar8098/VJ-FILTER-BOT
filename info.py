3554a808c5f838b6fa822a05e6cf1b18912e8175
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-10-15 18:55:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -132,7 +132,7 @@ YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998",
                            # Ask Doubt on telegram @KingVJ01
 
 
-# # Online Stream and Download
+MULTI_CLIENT = False
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
@@ -146,14 +146,13 @@ URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'TheBlackBot'))
 MULTI_CLIENT = False
 name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = str(getenv('APP_NAME'))
-
 else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
@@ -161,7 +160,6 @@ if HAS_SSL:
     URL = "https://{}/".format(FQDN)
 else:
     URL = "https://{}/".format(FQDN)
-
 
 # Rename Info : If True Then Bot Rename File Else Not
 RENAME_MODE = bool(environ.get('RENAME_MODE', False)) # Set True or False
